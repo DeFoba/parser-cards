2fa58fb9ec4f4d13a384f7317a22761f65d298fb
DeFoba <51561763+DeFoba@users.noreply.github.com>
2024-06-12 21:59:23 +0300

    Add new method & More changes

--- modules/headers.py
+++ modules/headers.py
@@ -4,5 +4,28 @@ DEFAULT_HEADERS = {
     'User-Agent': UserAgent().firefox
 }
 
+HEADERS = {
+    'irecommend.ru': {
+        'headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+            # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Connection': 'keep-alive',
+            'Cookie': 'ssu=1717654116506613825; captcha_check=1_1718187009_3b313a86_2YLTryBShg6UkosKQmEwtwTX0QY=; ab_var=12; ss_uid=1717654116506613825; statsactivity=2; statstimer=1',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Priority': 'u=0, i',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+        }
+        
+    }
+}
+
+
 if __name__ == '__main__':
     print(DEFAULT_HEADERS)
--- new_verse.py
+++ new_verse.py
@@ -5,16 +5,17 @@ from threading import Thread
 from datetime import datetime
 from os import listdir, mkdir
 from time import sleep
-from modules.headers import DEFAULT_HEADERS
+from modules.headers import DEFAULT_HEADERS, HEADERS
 from fp import fp
 import csv
 import json
+# from selenium import webdriver
+import undetected_chromedriver as uc
 
 PROX = fp.FreeProxy(rand=True)
 
-TEST = ['https://irecommend.ru/content/tinkoff-biznes', 'https://irecommend.ru/content/tinkoff', 'https://развивай.рф/business_credits/tinkoff/otzyvy',
-        'https://рко.рф/reviews/bank/tinkoff', 'https://bankiclub.ru/rkos/tinkoff-business/reviews/', 'https://bankiros.ru/bank/tcs/otzyvy',
-        'https://bankiros.ru/bank/tcs/otzyvy/rko', 'https://brobank.ru/banki/tinkoff/comments/', 'https://brobank.ru/rko-tinkoff/comments/', 
+TEST = [
+        'https://brobank.ru/banki/tinkoff/comments/', 'https://brobank.ru/rko-tinkoff/comments/', 
         'https://credits-on-line.ru/rko/tinkoff-biznes/otzyvy/', 'https://moskva.bankiros.ru/bank/tcs/otzyvy/rko', 'https://otzovik.com/reviews/tinkoff_biznes/',
         'https://ru.myfin.by/bank/tcs/otzyvy', 'https://topbanki.ru/banks/tcsbank/']
 
@@ -36,7 +37,382 @@ translate_month = {
     'Дек': 'December'
 }
 
+config = {
+    'irecommend.ru': {
+        'url':      'https://irecommend.ru',
+        'check_date': True,
+        'selenium': False,
+        'date':     'normal',
+        'links':    'all',
+        'prefix':   None,
+        'headers': HEADERS['irecommend.ru']['headers'],
+        'pages': {
+            'url':      '?new=1&page=',
+            'start_count': 0,
+            'max':      ['li', 'class', 'pager-last last'],
+            'level':    'easy' # easy / hard / veryhard
+        },
+        'cards_list_element':   ['div', 'class', 'view-referenced-nodes'],
+        'cards_list':           ['li',  'class', 'item'],
+        'stars': '',
+        'elements': {
+            'date':     ['div',     'class',    'created'],
+            'name':     ['h1',      'class',    'largeHeader'],
+            'title':    ['div',     'class',    'reviewTitle'],
+            'text':     ['span',    'class',    'reviewTeaserText'],
+            'link':     ['a',       'class',    'reviewTextSnippet']
+        }
+    },
+
+    'развивай.рф': {
+        'url':      'https://развивай.рф',
+        'check_date': True,
+        'selenium': False,
+        'date':     'hard',
+        'date_spliter': [', ', -1],
+        'links':    'none',
+        'prefix':   None,
+        'headers': DEFAULT_HEADERS,
+        'pages': None,
+        'cards_list_element':   ['div', 'class', '.*Reviews-styles__ReviewsList.'],
+        'cards_list':           ['div',  'id', '.*_Review_.'],
+        'stars': '',
+        'elements': {
+            'date':     ['div',     'class',    '.*ReviewCard-styles__Author.'],
+            'name':     ['h1',      'class',    '.*SeoHeader_Header.'],
+            'title':    ['div',     'class',    '.*ReviewCard-styles__Title.'],
+            'text':     ['div',    'class',    '.*ReviewCard-styles__Text.'],
+            'link':     None
+        }
+    },
+
+    'рко.рф': {
+        'url':      'https://рко.рф',
+        'selenium': False,
+        'check_date': False,
+        'date':     'hard2', # easy / hard (1, 2, 3, 4) / veryhard
+        'date_second': 'li',
+        'links':    'all find',
+        'links_find': 'a',
+        'prefix':   None,
+        'headers': DEFAULT_HEADERS,
+        'pages': {
+            'url':      '?page=',
+            'start_count': 0,
+            'max':      ['ul', 'class', 'paginator'],
+            'index':    -2,
+            'second_element': 'li',
+            'level':    'hard' # easy / hard / veryhard
+        },
+        'cards_list_element':   ['div', 'class', 'reviews-list'],
+        'cards_list':           ['div', 'class', 'reviews-user'],
+        'stars': '',
+        'elements': {
+            'date':     ['ul',     'class',    'reviews-user__info'],
+            'name':     ['div',      'class',    'catalog-bank__title'],
+            'title':    ['div',     'class',    'reviews-user__title'],
+            'text':     ['div',    'class',    'reviews-user__answer'],
+            'link':     ['div',    'class',    'reviews-user__more']
+        }
+    },
+
+    'bankiclub.ru': {
+        'url':      'https://bankiclub.ru',
+        'selenium': False,
+        'check_date': True,
+        'date':     'normal', # normal / hard (1, 2, 3, 4) / veryhard
+        'links':    'none',
+        'prefix':   None,
+        'headers': DEFAULT_HEADERS,
+        'pages': None,
+        'cards_list_element':   ['div', 'id', 'JsReviews'],
+        'cards_list':           ['div', 'class', 'review-contaner'],
+        'stars': '',
+        'elements': {
+            'date':     ['div',     'class',    'review-contaner-date'],
+            'name':     ['div',      'class',    'firm-item-h2'],
+            'title':    None,
+            'text':     ['div',    'class',    'review-content'],
+            'link':     None
+        }
+    },
+
+    'bankiros.ru': {
+        'url':      'https://bankiros.ru',
+        'selenium': False,
+        'check_date': True,
+        'date':     'normal', # normal / hard (1, 2, 3, 4) / veryhard
+        'links':    'no all',
+        'links_find': 'a',
+        'prefix':   '?limit=100',
+        'headers': DEFAULT_HEADERS,
+        'pages': None,
+        'cards_list_element':   ['div', 'class', 'xxx-reviews__list'],
+        'cards_list':           ['div', 'class', 'xxx-reviews-card__body'],
+        'stars': '',
+        'second_name': 'li',
+        'name_index': -1,
+        'elements': {
+            'date':     ['span',     'class',    'xxx-reviews-info__date'],
+            'name':     ['ul',      'class',    'breadcrumb'],
+            'title':    ['div',      'class',    'xxx-reviews-card__title'],
+            'text':     ['p',    'class',    'xxx-reviews-card__content'],
+            'link':     ['div',      'class',    'xxx-reviews-card__title']
+        }
+    },
+
+    'brobank.ru': {
+        'url':      'https://brobank.ru',
+        'selenium': False,
+        'check_date': True,
+        'date':     'hard', # normal / hard (1, 2, 3, 4) / veryhard
+        'date_spliter': [' ', 0],
+        'links':    'none',
+        'prefix':   None,
+        'headers': DEFAULT_HEADERS,
+        'pages': {
+            'url':      '/comment-page-',
+            'start_count': 1,
+            'max':      ['div', 'class', 'navigation__list'],
+            'index':    -2,
+            'second_element': 'a',
+            'level':    'hard' # easy / hard / veryhard
+        },
+        'cards_list_element':   ['ol', 'id', 'comments-list_ajax'],
+        'cards_list':           ['article', 'class', 'comment'],
+        'stars': '',
+        'elements': {
+            'date':     ['time',     None,    None],
+            'name':     ['div',      'class',    'page-title'],
+            'title':    ['div',      'class',    'title_review'],
+            'text':     ['section',    'class',    'comment-content'],
+            'link':     None
+        }
+    },
+
+    # 'brobank.ru':
+    # 'credits-on-line.ru':
+    # 'ru.myfin.by':
+    # 'topbanki.ru'
+}
+
+
 class Parser:
+    def __init__(self):
+        self.stop_date = self.string_to_date('03.03.2021')
+        self.names = ['date', 'name', 'stars', 'links', 'text']
+        self.result = [self.names]
+
+    # Normalize date
+    def string_to_date(self, start_date):
+        if ' ' in start_date:
+            d, m, y = start_date.lower().split(' ')
+            m = translate_month[str(m).capitalize()[:3]][:3]
+
+            return datetime.strptime(' '.join([d, m, y]), u'%d %b %Y')
+        
+        if start_date != None and start_date != '': return datetime.strptime(start_date, '%d.%m.%Y')
+        else: return 'LAST'
+
+    # Date turn to string value
+    def date_to_string(self, date):
+        return datetime.strftime(date, '%d.%m.%Y')
+
+    # Check domain in URL 
+    def is_true_domain(sel, url:str):
+        for domain in config:
+            if domain in url:
+                return domain
+        return False
+    
+    # Normalize selection list
+    def selector(self, value:list):
+        tag, select, val = value
+
+        if select == None: return [tag, None]
+        if '.*' in val: return [tag, {select: re.compile(val)}]
+
+        return [tag, {select: val}]
+    
+    # Find count pages
+    def find_max_pages(self, domain, html):
+        max_page = 0
+
+        match config[domain]['pages']['level']:
+            case 'easy': max_page = int(html.find(*self.selector(config[domain]['pages']['max'])).text)
+            case 'hard': max_page = int(html.find(*self.selector(config[domain]['pages']['max'])).find_all(config[domain]['pages']['second_element'])[config[domain]['pages']['index']].text)
+            case 'veryhard': max_page = int(0)
+
+        return max_page
+    
+    # Remove empty spaces in data
+    def normalize_data(self, value:list):
+        return [str(val).strip().replace('\n', ' ').replace('\r', ' ') for val in value]
+    
+    # Parsing cards on page
+    def collect_cards(self, domain, url, html, block):
+        if not 'second_name' in config[domain]:
+            NAME = html.find(*self.selector(config[domain]['elements']['name'])).text
+        else:
+            NAME = html.find(*self.selector(config[domain]['elements']['name'])).find_all(config[domain]['second_name'])[config[domain]['name_index']].text
+
+        STOP = False
+
+        for card in block.find_all(*self.selector(config[domain]['cards_list'])):
+            match config[domain]['date']:
+                case 'normal':  DATE = self.date_to_string(self.string_to_date(card.find(*self.selector(config[domain]['elements']['date'])).text))
+                case 'hard':    DATE = self.date_to_string(self.string_to_date(card.find(*self.selector(config[domain]['elements']['date'])).text.strip().replace('\n', ' ').replace('\r', ' ').split(config[domain]['date_spliter'][0])[config[domain]['date_spliter'][-1]]))
+                case 'hard2':   DATE = self.date_to_string(self.string_to_date(card.find(*self.selector(config[domain]['elements']['date'])).find(config[domain]['date_second']).text.strip().replace('\n', ' ').replace('\r', ' ')))
+
+
+            STARS = 0
+            TEXT = ''
+            LINK = url
+            add_to_result = True
+
+            match config[domain]['links']:
+                case 'all':
+                    LINK = config[domain]['url'] + card.find(*self.selector(config[domain]['elements']['link'])).get('href')
+
+                case 'all find':
+                    LINK = config[domain]['url'] + card.find(*self.selector(config[domain]['elements']['link'])).find(config[domain]['links_find']).get('href')
+
+                case 'not all':
+                    find_link = card.find(*self.selector(config[domain]['elements']['link'])).find(config[domain]['links_find'])
+
+
+                    if find_link != None:
+                        LINK = config[domain]['url'] + find_link.get('href')
+
+
+            if config[domain]['elements']['title'] != None:
+                try:
+                    TEXT = card.find(*self.selector(config[domain]['elements']['title'])).text + ' ' + card.find(*self.selector(config[domain]['elements']['text'])).text
+                except:
+                    continue
+            else:
+                TEXT = card.find(*self.selector(config[domain]['elements']['text'])).text
+
+            if self.string_to_date(DATE) < self.stop_date:
+                if config[domain]['check_date']:
+                    STOP = True
+                    break
+                else:
+                    add_to_result = False
+
+            # star_count = 0
+            # for star in card.find_all('div', {'class': 'star'}):
+            #     d_star = star.find('div')
+            #     if d_star.get('class')[0] == 'on': star_count += 1
+
+            if add_to_result:
+                self.result.append(self.normalize_data([DATE, NAME, STARS, LINK, TEXT]))
+
+        return STOP
+
+    def run_loop_of_pages(self, domain, url):
+        current_page = config[domain]['pages']['start_count']
+
+        html, card_links_element = self.get_page(domain, url + config[domain]['pages']['url'] + str(current_page))
+        sleep(2)
+
+        # with open('index.html', 'w', encoding='utf-8') as file:
+        #     file.write(str(html))
+
+        if self.collect_cards(domain, url, html, card_links_element): return
+
+
+        current_page += 1
+
+
+        max_page = self.find_max_pages(domain, html)
+
+        while current_page < max_page:
+            html, card_links_element = self.get_page(domain, url + config[domain]['pages']['url'] + str(current_page))
+            if self.collect_cards(domain, url, html, card_links_element): break
+            current_page += 1
+            sleep(2)
+
+    # Send request and get html content
+    def get_page(self, domain, url):
+        if config[domain]['prefix'] != None:
+            url = url + config[domain]['prefix']
+
+        if not config[domain]['selenium']:
+            response = requests.get(url, headers=config[domain]['headers'], proxies=None).content
+            sleep(0.1)
+
+        print(url)
+
+        html = BeautifulSoup(response, 'html.parser')
+        sleep(0.1)
+
+        card_list = html.find(*self.selector(config[domain]['cards_list_element']))
+
+        return html, card_list
+
+    def run_no_pages(self, domain, url):
+        self.collect_cards(domain, url, *self.get_page(domain, url))
+
+
+
+
+    # Pars cards by URL
+    def pars_by_request(self, url):
+        domain = self.is_true_domain(url)
+        if domain == False: return
+
+        if config[domain]['pages'] == None: self.run_no_pages(domain, url)
+        else: self.run_loop_of_pages(domain, url)
+            
+
+
+if __name__ == '__main__':
+    parser = Parser()
+    # parser.pars_by_request('https://irecommend.ru/content/tinkoff-biznes') # FIX IT: <-------------
+    # parser.pars_by_request('https://развивай.рф/business_credits/tinkoff/otzyvy')
+    # parser.pars_by_request('https://рко.рф/reviews/bank/tinkoff')
+    # parser.pars_by_request('https://bankiclub.ru/rkos/tinkoff-business/reviews')
+    # parser.pars_by_request('https://bankiros.ru/bank/tcs/otzyvy/rko')
+    parser.pars_by_request('https://brobank.ru/banki/tinkoff/comments')
+    print(parser.result)
+
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Parser2:
     def __init__(self, links:list, start_date:str=None):
         self.start_date = self.string_to_date(start_date)
         self.domains, self.last_dates, self.result = {}, {}, []
@@ -153,7 +529,7 @@ class Parser:
         name = html.find('h1', {'class': re.compile('.*SeoHeader_Header.')}).text.strip().replace('\n', ' ')
 
         main_block = html.find('div', {'class': re.compile('.*Reviews-styles__ReviewsList.')})
-        for card in main_block.find_all('div', {'id': re.compile('.*tinkoff_Review.')})[::-1]:
+        for card in main_block.find_all('div', {'id': re.compile('.*_Review_.')})[::-1]:
             # cards.append()
             data = []
 
@@ -741,9 +1117,10 @@ class Parser:
 
 
 if __name__ == '__main__':
-    pars = Parser(TEST, '23.05.2023')
+    # pars = Parser(TEST, '23.05.2023')
+    pass
     # for link in TEST:
-    pars.irecommend(TEST[0])
+    #     pars.irecommend_parsing_card(link)
 
     # pars.razvivay(TEST[2])
 
@@ -767,6 +1144,6 @@ if __name__ == '__main__':
 
 
 
-    pars.save_result()
+    # pars.save_result()
 
     # print(pars.__dict__)
